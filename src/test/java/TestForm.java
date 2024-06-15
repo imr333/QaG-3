3ca630e79ed19f38eaf2953689f42daf52762c93
imr333 <rror666@mail.ru>
2024-06-15 13:28:52 +0300

    update

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -2,13 +2,14 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestForm {
     @BeforeAll
     static void beforeAll() {
-        open("https://demoqa.com");
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
 
@@ -16,15 +17,14 @@ public class TestForm {
 
     @Test
     void testForm() {
-        open("/automation-practice-form");
 
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("Rory");
         $("#lastName").setValue("McDonald");
         $("#userEmail").setValue("333@gmail.com");
         $("#userNumber").setValue("1234567890");
         $("#currentAddress").setValue("Test 1");
-
-
     }
 
 }
